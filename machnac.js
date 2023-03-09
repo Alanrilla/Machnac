dafb1a6609942ffeb3896c880a4c22df856bc4de
Kayla <103388509+Alanrilla@users.noreply.github.com>
2023-03-09 10:09:06 +0100

    Add files via upload
    Modified the akcie section to Zabava v Machnaci. 
    Added banner for Aktualne Informacie.
    Added JS for close button for Aktualne Informacie. 
    Modified and aligned the animation (small line under headings) to better with a screen with rem instead of %.

--- machnac.js
+++ machnac.js
@@ -6,6 +6,18 @@ menu.onclick = () => {
   navbar.classList.toggle("active");
 };
 
+
+// --------------------------------------------------JavaScript for scroll effect to full in the color of navbar from being transparent------------------------------------------------------------------------
+
 window.addEventListener("scroll", () => {
   document.querySelector("header").classList.toggle("header-scroll", window.scrollY > 830);
 });
+// --------------------------------------------------JavaScript for X button for banner section------------------------------------------------------------------------
+
+document.querySelector(".aktualne-informacie-button").addEventListener("click", function () {
+  this.closest(".aktualne-informacie").style.display = "none";
+});
+
+
+
+
